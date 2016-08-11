84bb3940ab03befc4da97f19ba53797f248a9394
Luiz Menezes <luiz.menezesf@gmail.com>
2016-08-11 01:49:12 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+with open("requirements.txt") as reqs:
+    install_requires = reqs.readlines()
+
+
+setup(
+    name="dojo-toolkit",
+    version="0.1.0",
+    url="https://github.com/grupy-sanca/dojo-toolkit",
+
+    author="Grupy-Sanca",
+
+    description="A Toolkit for Python Coding Dojos",
+    long_description=open('README.md').read(),
+
+    packages=find_packages(),
+
+    install_requires=install_requires,
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
