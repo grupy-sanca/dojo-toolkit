50e5c65db3910735fbd887ad0455f521772c67a6
Luiz Menezes <luiz.menezesf@gmail.com>
2016-08-16 22:18:08 -0300

    fix input for py2.7

--- dojo_toolkit/timer.py
+++ dojo_toolkit/timer.py
@@ -1,13 +1,18 @@
 import time
 
 
+try:
+    input = raw_input
+except NameError:
+    pass
+
+
 def dojo_timer(notifier, round_time):
     """Wait the defined and then shows notification and waits
     for replace
     """
     while True:
         notifier.notify('Time Up', timeout=15 * 1000)
-        print("\033c")
         print('Press Enter when replaced')
-        raw_input()
+        input()
         time.sleep(round_time * 60)
