34df27071231f85c39d84d22771c65334f9211a9
Douglas Shimada <shimadadouglas@gmail.com>
2016-08-23 06:05:33 -0300

    remove --path, rename argument_parser to main.py

--- dojo_toolkit/dojo.py
+++ dojo_toolkit/dojo.py
@@ -7,14 +7,12 @@ from dojo_toolkit.code_handler import DojoCodeHandler
 from dojo_toolkit.notifier import GnomeNotifier
 from dojo_toolkit.test_runner import DoctestTestRunner
 from dojo_toolkit.timer import dojo_timer
-from dojo_toolkit.argument_parser import build_parser
 
 
 class Dojo:
 
     def __init__(self, code_path, round_time=5, notifier=None, test_runner=None):
-        print('Watching: {}\nTo change, reopen with path in first argument'.format(
-            code_path))
+        print('Watching: {}\nTo change, reopen with path in first argument'.format(code_path))
 
         self.round_time = round_time
 
@@ -24,13 +22,11 @@ class Dojo:
         if not test_runner:
             test_runner = DoctestTestRunner(code_path=code_path)
 
-        self.event_handler = DojoCodeHandler(
-            notifier=notifier, test_runner=test_runner)
+        self.event_handler = DojoCodeHandler(notifier=notifier, test_runner=test_runner)
         self.observer = Observer()
         self.observer.schedule(self.event_handler, code_path, recursive=False)
 
-        self.timer_thread = threading.Thread(
-            target=dojo_timer, args=(notifier, self.round_time))
+        self.timer_thread = threading.Thread(target=dojo_timer, args=(notifier, self.round_time))
 
     def start(self):
         self.observer.start()
@@ -41,15 +37,3 @@ class Dojo:
         except KeyboardInterrupt:
             self.observer.stop()
         self.observer.join()
-
-
-def main():
-    args = build_parser().parse_args()
-    round_time = args.time
-    code_path = args.path
-    dojo = Dojo(code_path, round_time)
-    dojo.start()
-
-
-if __name__ == "__main__":
-    main()
---
+++ dojo_toolkit/main.py
@@ -0,0 +1,33 @@
+import argparse
+import os
+from dojo_toolkit.dojo import Dojo
+
+ROUND_TIME = 5
+
+
+def parse_args():
+    """
+    build argument parser
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--time",
+                        help="dojo time",
+                        default=ROUND_TIME,
+                        type=float)
+    parser.add_argument("path",
+                        help="code path",
+                        nargs="?",
+                        default=".",
+                        type=os.path.realpath)
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    dojo = Dojo(args.path, args.time)
+    dojo.start()
+
+
+if __name__ == "__main__":
+    main()
