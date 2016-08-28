e06ae9e6a7dd74ed2eda1781b33ccfb03aee4c8f
Luiz Menezes <luiz.menezesf@gmail.com>
2016-08-28 11:25:41 -0300

    add Dojo and DojoCodeHandler tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,29 @@
+from unittest import mock
+
 import pytest
 
+from dojo_toolkit.code_handler import DojoCodeHandler
 from dojo_toolkit.notifier import GnomeNotifier
+
+
+@pytest.fixture
+def mocked_code_handler():
+    code_handler = DojoCodeHandler(notifier=mock.Mock(),
+                                   test_runner=mock.Mock(),
+                                   sound_player=mock.Mock())
+    return code_handler
 
 
 @pytest.fixture
 def gnome_notifier():
     return GnomeNotifier()
+
+
+@pytest.fixture
+def mocked_gnome_notifier():
+    with mock.patch('dojo_toolkit.notifier.Notify') as mock_notify, \
+         mock.patch('dojo_toolkit.notifier.GdkPixbuf') as gdk_pixbuf:
+        gnome_notifier = GnomeNotifier()
+        gnome_notifier.mock_notify = mock_notify
+        gnome_notifier.mock_gdk_pixbuf = gdk_pixbuf
+        return gnome_notifier
