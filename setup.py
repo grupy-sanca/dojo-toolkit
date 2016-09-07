53f37e89ca061eb9c5569a9b6756cb4be5172bf9
Luiz Menezes <luiz.menezesf@gmail.com>
2016-09-07 14:03:14 -0300

    Bump version: 0.3.2 → 0.3.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ package_data = {
 
 setup(
     name="dojo-toolkit",
-    version="0.3.2",
+    version="0.3.3",
     url="https://github.com/grupy-sanca/dojo-toolkit",
 
     author="Grupy-Sanca",
