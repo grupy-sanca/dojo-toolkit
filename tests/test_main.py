23a7e32783668b23d5ca3069a1a1d66fc27bc6b6
Luiz Menezes <luiz.menezesf@gmail.com>
2016-09-19 08:54:11 -0300

    add tests

---
+++ tests/test_main.py
@@ -0,0 +1,15 @@
+from dojo_toolkit.main import main, parse_args
+from tests.conftest import mock
+
+
+@mock.patch('dojo_toolkit.main.ArgumentParser')
+def test_main_parse_args(argument_parser):
+    assert parse_args()
+    assert argument_parser.called
+
+
+@mock.patch('dojo_toolkit.main.ArgumentParser')
+@mock.patch('dojo_toolkit.main.Dojo')
+def test_main_main(dojo, argparser):
+    main()
+    assert dojo.called
