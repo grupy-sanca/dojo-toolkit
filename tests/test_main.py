0c4762c6b579dcb1973cca89b00c74f67793d332
Luiz Menezes <luiz.menezesf@gmail.com>
2016-11-26 17:33:29 -0200

    Remove main.py (move code to __main__)

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,8 @@
-from dojo_toolkit.main import main, parse_args
+from dojo_toolkit.__main__ import parse_args
 from dojo_toolkit.utils import mock
 
 
-@mock.patch('dojo_toolkit.main.ArgumentParser')
+@mock.patch('dojo_toolkit.__main__.ArgumentParser')
 def test_main_parse_args(argument_parser):
     assert parse_args()
     assert argument_parser.called
-
-
-@mock.patch('dojo_toolkit.main.ArgumentParser')
-@mock.patch('dojo_toolkit.main.Dojo')
-def test_main_main(dojo, argparser):
-    main()
-    assert dojo.called
