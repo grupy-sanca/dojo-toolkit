0e61fc5e78320d9ffab5ce85099d8a2b7b436b91
Ana Vitoria Gouvêa de Oliveira Freitas <69052949+AnaVitoriaFreitasUSP@users.noreply.github.com>
2020-10-24 19:42:25 -0300

    fix Notify warning (#60)
    * fix Notify warning
    
    * Fix Watchdog version on requirements.txt

--- dojo_toolkit/notifier.py
+++ dojo_toolkit/notifier.py
@@ -5,7 +5,9 @@ from .settings import ASSETS_DIR
 
 # workaround to tests run on travis
 try:
-    from pgi.repository import Notify, GdkPixbuf
+    import gi
+    gi.require_version('Notify', '0.7')
+    from gi.repository import Notify, GdkPixbuf
 except ImportError:
     Notify = mock.Mock()
     GdkPixbuf = mock.Mock()
