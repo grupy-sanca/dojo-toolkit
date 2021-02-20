1901460e66a75648460ffa853c8ec30b792a4d70
zsinx6 <zsinx6@users.noreply.github.com>
2021-02-20 11:19:36 -0300

    Fix PEP 8
    Co-authored-by: anadulce <anadulcepadovan@gmail.com>

--- dojo_toolkit/discord_handler.py
+++ dojo_toolkit/discord_handler.py
@@ -4,9 +4,10 @@ import discord
 
 client = discord.Client()
 
+
 async def send_message(message):
     channel = client.get_channel(os.getenv('ID'))
-    await channeld.send(message)
+    await channel.send(message)
 
 
 client.run(os.getenv('TOKEN'))
