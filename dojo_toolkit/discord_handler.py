8bffb0f9aeadb0cf56b433f99a65231de3062a22
Luiz Menezes <luiz.menezesf@gmail.com>
2021-02-21 17:18:33 -0300

    Schedule tasks on discord.py client eventloop from a different thread

--- dojo_toolkit/discord_handler.py
+++ dojo_toolkit/discord_handler.py
@@ -1,13 +1,31 @@
+import asyncio
+import threading
 import os
 
 import discord
 
+DISCORD_TOKEN = os.getenv('TOKEN')
 client = discord.Client()
 
 
 async def send_message(message):
-    channel = client.get_channel(os.getenv('ID'))
-    await channel.send(message)
+    print('sendou message')
+    await asyncio.sleep(2)
+    print('top', message)
+    # channel = client.get_channel(os.getenv('ID'))
+    # await channel.send(message)
 
 
-client.run(os.getenv('TOKEN'))
+def foo():
+    import time
+    print('foo')
+    time.sleep(2)
+    client.loop.create_task(send_message('show'))
+    client.loop.create_task(send_message('top'))
+    client.loop.create_task(send_message('pot'))
+
+
+thread = threading.Thread(target=foo)
+thread.start()
+
+client.run(DISCORD_TOKEN)
