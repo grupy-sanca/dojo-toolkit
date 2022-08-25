a343a9ac79f2e8045e795c075fa805d57db3c297
zsinx6 <zsinx6@users.noreply.github.com>
2022-08-25 09:28:23 -0300

    Use typer

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,29 @@
 from unittest import mock
 
-from dojo_toolkit.main import parse_args
+import typer
+from typer.testing import CliRunner
+
+from dojo_toolkit.main import main, run
+
+app = typer.Typer()
+app.command()(main)
+runner = CliRunner()
 
 
-@mock.patch("dojo_toolkit.main.ArgumentParser")
-def test_main_parse_args(argument_parser):
-    assert parse_args()
-    assert argument_parser.called
+@mock.patch("dojo_toolkit.main.Dojo")
+def test_main(mock_dojo):
+    result = runner.invoke(app)
+    assert result.exit_code == 0
+    mock_dojo.assert_called_once_with(
+        code_path=".",
+        round_time=5.0,
+        mute=False,
+        runner="doctest",
+    )
+    mock_dojo.return_value.start.assert_called_once_with()
+
+
+@mock.patch("dojo_toolkit.main.typer")
+def test_run(mock_typer):
+    run()
+    mock_typer.run.assert_called_once_with(main)
