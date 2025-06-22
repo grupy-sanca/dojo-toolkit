5b7a38c2810bb5993a31741709d83d4b615ea7fb
Danilo Martins <mawkee@gmail.com>
2025-06-22 12:07:35 -0300

    Enable sound playing on mac (#98)
    * Enable sound playing on mac
    
    * Remove unused import

--- dojo_toolkit/dojo.py
+++ dojo_toolkit/dojo.py
@@ -1,4 +1,3 @@
-import sys
 from threading import Thread
 from unittest import mock
 
@@ -17,7 +16,7 @@ class Dojo:
     def __init__(self, code_path, round_time=None, mute=False, test_runner=None, runner="doctest"):
         self.code_path = code_path
         self.round_time = round_time or self.ROUND_TIME
-        self.sound_player = mock.Mock() if mute or sys.platform != "linux" else SoundHandler()
+        self.sound_player = mock.Mock() if mute else SoundHandler()
         self.info_notified = False
         self.timer = Timer(self.round_time)
 
