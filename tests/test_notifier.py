d100b9bf2bd0a099149040c54c33b29a7be258b6
Enzzo Cavallo <souenzzo@gmail.com>
2026-06-04 13:40:37 -0300

    +coverrage

--- tests/test_notifier.py
+++ tests/test_notifier.py
@@ -6,5 +6,11 @@ from dojo_toolkit.notifier import Notifier
 @mock.patch("desktop_notifier.sync.DesktopNotifierSync.send")
 def test_notifyier(send):
     n = Notifier()
-    n.notify("message")
-    assert send.call_args_list == [mock.call(title='dojo toolkit', message='message', timeout=300)]
+    n.notify("a")
+    n.success("b")
+    n.fail("c")
+    assert send.call_args_list == [
+        mock.call(title='dojo toolkit', message='a', timeout=300),
+        mock.call(title='dojo success', message='b', icon=n.SUCCESS_ICON),
+        mock.call(title='dojo fail', message='c', icon=n.FAIL_ICON)
+    ]
